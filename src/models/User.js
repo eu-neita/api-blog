f8f69bfa04f8531fc44c74657d1b534004bfa472
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-07-13 17:17:12 -0300

    feat: UserModel create

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
