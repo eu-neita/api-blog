182be4e44765e65fe1f5fef67e80a3aa5b9e6044
Neita <neita-macos@Neitas-iMac-Pro.local>
2023-07-14 00:02:44 -0300

    feat: add route /user and function to add new user to db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { loginServices, userCreateServices } = require('./services');
 const loginMiddleware = require('./middlewares/login.midleware');
+const userCreateMiddleware = require('./middlewares/userCreate.middleware');
 
 // ...
 
@@ -14,7 +15,7 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 app.post('/login', loginMiddleware, loginServices);
-app.post('/user', userCreateServices);
+app.post('/user', userCreateMiddleware, userCreateServices);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
